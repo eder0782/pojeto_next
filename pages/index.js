0ff97bd60158570624f8fddf08f76c6184708e43
eder0782 <eder0782@gmail.com>
2022-07-16 07:50:03 -0400

    início

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import SignIn from '../components/SignIn'
+
+export default function Home() {
+  return (
+    <>
+      <SignIn />
+    
+    </>
+  )
+}
