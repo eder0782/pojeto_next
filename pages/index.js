a5b215e69d938bc88d4edcaa8e0e6e2022ca8346
eder0782 <eder0782@gmail.com>
2022-07-18 08:21:03 -0400

    adicionada a tag meta, para corrigir o problema da requisição http para a api ao inves de https

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import SignIn from '../components/SignIn'
 
+
+
 export default function Home() {
   return (
     <>
-		
+      <Head>
+      
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
+      </Head>
       <SignIn />
     
     </>
