371586f747b5dbdcf359e7cb2619165a8b468a8a
eder0782 <eder0782@gmail.com>
2022-07-20 09:39:17 -0400

    criada a tela home, navbar e footer

---
+++ pages/home2.js
@@ -0,0 +1,23 @@
+
+import { Box } from "@chakra-ui/react";
+import NavBar from "../components/NavBar";
+import SmallCentered from "../components/Footer";
+
+
+function Home2() {
+
+    return(
+        <Box height='100vh'>
+        <NavBar>
+
+        </NavBar>
+
+        <SmallCentered />
+
+
+        </Box>
+    )
+    
+}
+
+export default Home2;
