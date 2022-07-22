30badbdb8a6921ee933c4b7b41ecb85edb79b205
eder0782 <eder0782@gmail.com>
2022-07-22 10:57:24 -0400

    adicinado a tag head global com uma tag meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import '../styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
   return (
   <ChakraProvider>
+    <Head>
+      
+      <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests"/> 
+      </Head>
     <Component {...pageProps} />
   </ChakraProvider>
   )
